c725560a0f6ee53ea1410faec84c208def847f31
Liry <learainccc@outlook.com>
2026-04-13 23:06:36 +0800

    feat: update tsconfig and vite config with path aliases

--- packages/luna/vite.config.ts
+++ packages/luna/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
@@ -5,6 +6,12 @@ const viteConfig = defineConfig({
   plugins: [solid()],
   build: {
     target: false
+  },
+  resolve: {
+    alias: {
+      '@': resolve(import.meta.dirname, 'src'),
+      '@@': resolve(import.meta.dirname)
+    }
   },
   server: {
     port: 26413
